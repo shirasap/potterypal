6f71375c0372a8ff14d6bc805ec719762071a80d
Shirasa Prajapati <shirasapraja@gmail.com>
2024-06-10 13:23:45 -0600

    add more data to the seed files.

--- server/seed-data/images.js
+++ server/seed-data/images.js
@@ -2,22 +2,26 @@ module.exports=[
     {
         id:1,
         piece_id: 1,
-        img_name: "leaf-mug"    
+        img_name: "leaf-mug.jpg"    
     },
     {
         id:2,
-        piece_id: 1,
-        img_name: "espresso-mug"
-    
+        piece_id: 2,
+        img_name: "coil-pot.jpg"
     },
     {
         id:3,
-        piece_id: 2,
-        img_name: "coil-pot"
+        piece_id: 3,
+        img_name: "slip-swirl-trimmed.jpg"
     },
     {
         id:4,
-        piece_id: 3,
-        img_name: "yellow-slip-bowl"
+        piece_id: 4,
+        img_name: "handbuilt-flower.jpg"
+    },
+    {
+        id:5,
+        piece_id: 5,
+        img_name: "blue-stain-swirl.jpg"
     }
 ]
--- server/seed-data/pieces.js
+++ server/seed-data/pieces.js
@@ -21,10 +21,28 @@ module.exports=[
     },
     {
         id:3,
-        user_id: 2,
-        title: "Bowl",
+        user_id: 1,
+        title: "Slip Bowls",
         clay_type: "red",
-        stage: "bisqued",
-        description: "Wheel thrown bowl with white slip base and spiraled yellow slip on top."
+        stage: "trimmed",
+        description: "Wheel thrown bowls with white slip base and spiraled coloured slip on top."
+    },
+    {
+        id:4,
+        user_id: 1,
+        title: "Handbuilt Flower Bowl",
+        clay_type: "red",
+        stage: "glazed",
+        description: "Handbuilt bowl with flower imprints. Imprints stained with green stain",
+        glaze: "Transparent glaze"
+    },
+    {
+        id:5,
+        user_id: 1,
+        title: "Spiral stain bowls",
+        clay_type: "white",
+        stage: "glazed",
+        description: "Wheel thrown bowls with blue and grey stain.",
+        glaze: "Transparent glaze"
     }
 ]
