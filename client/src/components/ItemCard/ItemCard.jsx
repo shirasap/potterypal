f5af35c237557f6a3edb9f4fc4e1134548970301
Shirasa Prajapati <shirasapraja@gmail.com>
2024-06-12 00:07:15 -0600

    add delete modal and edit icon to every item card

--- client/src/components/ItemCard/ItemCard.jsx
+++ client/src/components/ItemCard/ItemCard.jsx
@@ -1,16 +1,40 @@
 import { Link } from "react-router-dom";
 import "./ItemCard.scss";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteModal from "../DeleteModal/DeleteModal";
 
 export default function ItemCard({ entry }) {
   return (
     <>
-      <Link to={`/piece/${entry.piece_id}`}>
+      <div className="piece">
         <div className="piece__card">
-          <h1 className="piece__title">{entry.title}</h1>
-          {entry.images ? <img src={`${import.meta.env.VITE_LOCALHOST}/images/${entry.images}`} alt="piece image" className="piece__img" /> : null}
-          <p className="piece__description">{entry.description}</p>
+          <Link to={`/piece/${entry.piece_id}`}>
+            <div>
+              {entry.images ? (
+                <div className="piece__img-container">
+                  {" "}
+                  <img
+                    src={`${import.meta.env.VITE_LOCALHOST}/images/${
+                      entry.images
+                    }`}
+                    alt="piece image"
+                    className="piece__img"
+                  />{" "}
+                </div>
+              ) : null}
+              <div className="piece__info">
+                <h2 className="piece__title">{entry.title}</h2>
+              </div>
+            </div>
+          </Link>
+          <div className="piece__actions">
+            <Link to={`/piece/edit/${entry.piece_id}`} className="piece__edit">
+              <EditIcon fontSize="small" />
+            </Link>
+            <DeleteModal pieceId={entry.piece_id} piece={entry} className="piece__delete"/>
+          </div>
         </div>
-      </Link>
+      </div>
     </>
   );
 }
