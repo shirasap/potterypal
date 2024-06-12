61b89082680ef342aa48eb04172b2f6c90ad5b92
Shirasa Prajapati <shirasapraja@gmail.com>
2024-06-12 00:04:16 -0600

    style and add contact links to footer.

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
+import "./Footer.scss"
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <>
-      <p>Footer</p>
+      <div className="footer">
+        <div className="footer__socials">
+          <h2 className="footer__heading">Get in touch</h2>
+          <Link to="https://www.linkedin.com/in/shirasa-p-300486133/">
+          <LinkedInIcon />
+          </Link>
+          <Link to="https://github.com/shirasap">
+          <GitHubIcon/>
+          </Link>
+
+        </div>
+        <p className="footer__text">© PotteryPal. All Rights Reserved.</p>
+      </div>
     </>
   );
 }
